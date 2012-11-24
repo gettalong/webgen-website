ea8850b400de26104624bce2d1d1fb5d5bb01ed7
Thomas Leitner <t_leitner@gmx.at>
2012-11-24 08:22:18 +0100

    Now also showing superfluous content processor pages

--- ext/init.rb
+++ ext/init.rb
@@ -8,7 +8,8 @@ require_relative('kramdown_adaptions')
 
 website.blackboard.add_listener(:website_generated) do
   # check if all content processors are documented with correct infos
-  nodes = website.ext.node_finder.find({:alcn => '/documentation/reference/content_processor/*.html', :flatten => true},
+  nodes = website.ext.node_finder.find({:alcn => '/documentation/reference/content_processor/*.html', :flatten => true,
+                                         :not => {:alcn => '/**/index.html'}},
                                        website.tree.root)
   website.ext.content_processor.registered_extensions.each do |name, data|
     node = nodes.find {|n| n.cn == "#{name}.html"}
@@ -18,5 +19,11 @@ website.blackboard.add_listener(:website_generated) do
     if node && node['title'] != "content_processor.#{name}"
       website.logger.warn { "Node title for documentation of content processor '#{name}' not correct" }
     end
+    nodes.delete(node) if node
+  end
+
+  if !nodes.empty?
+    nodes = nodes.map {|n| n.cn.sub(/\.html$/, '') }.join(', ')
+    website.logger.warn { "Documentation pages found for unknown content processors: #{nodes}" }
   end
 end
