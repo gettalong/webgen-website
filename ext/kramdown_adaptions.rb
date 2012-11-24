ade8b7d5159e958aa3d2ac270c8cfb291e8ad200
Thomas Leitner <t_leitner@gmx.at>
2012-11-24 08:39:17 +0100

    Fixed output of shell code by using correct html escaping

--- ext/kramdown_adaptions.rb
+++ ext/kramdown_adaptions.rb
@@ -6,10 +6,13 @@ class Webgen::ContentProcessor::Kramdown::CustomHtmlConverter
     attr = el.attr.dup
     lang = extract_code_language!(attr)
     if lang == 'shell'
-      result = el.value.strip
-      result.gsub!(/^\$ (.*?)$/) do
-        "<span class=\"prompt\">$</span> <kbd>#{$1}</kbd>"
-      end
+      result = el.value.strip.lines.map do |line|
+        if line =~ /^(\$|C:\\>) (.*)$/
+          "<span class=\"prompt\">#{escape_html($1)}</span> <kbd>#{escape_html($2)}</kbd>"
+        else
+          escape_html(line)
+        end
+      end.join("\n")
       "#{' '*indent}<pre#{html_attributes(attr)}>#{result}</pre>\n"
     else
       super
