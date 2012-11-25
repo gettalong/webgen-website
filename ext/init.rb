1b0394bf18168b8f47bf4d6813171319efb5216a
Thomas Leitner <t_leitner@gmx.at>
2012-11-25 13:32:01 +0100

    Added custom content processor for inserting link definitions into page content

--- ext/init.rb
+++ ext/init.rb
@@ -27,3 +27,33 @@ website.blackboard.add_listener(:website_generated) do
     website.logger.warn { "Documentation pages found for unknown content processors: #{nodes}" }
   end
 end
+
+
+ref_links = "\n\n"
+website.blackboard.add_listener(:after_tree_populated) do
+  BundleInfos.bundles(website).each do |name, infos|
+    next unless name.include?('.')
+    alcn = '/documentation/reference/' << name.sub(/\./, '/') << ".en.html"
+    if !website.tree[alcn]
+      website.logger.warn { "No documentation page for '#{name}' at <#{alcn}> found" }
+    end
+    desc = "'#{infos['summary'].tr("\n", ' ')}'"
+    ref_links << "[#{name}]: #{alcn} #{desc}\n"
+    name1, name2 = name.split('.')
+    ref_links << "[#{name1.tr('_', ' ')} #{name2}]: #{alcn} #{desc}\n"
+  end
+  ref_links << "\n"
+  website.config.options.each do |name, option|
+    alcn = '/documentation/reference/config_options.en.html#' << name.tr('_.', '')
+    if !website.tree[alcn]
+      website.logger.warn { "No documentation page for '#{name}' at <#{alcn}> found" }
+    end
+    desc = option.description.include?('"') ? '' : " \"#{option.description}\""
+    ref_links << "[#{name}]: #{alcn}#{desc}\n"
+  end
+end
+
+website.ext.content_processor.register('link_defs') do |context|
+  context.content << ref_links
+  context
+end
